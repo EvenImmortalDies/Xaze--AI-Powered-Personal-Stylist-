643a7cf620e06582e30dd484824844bdc7aa481a
Amar <88706238+EvenImmortalDies@users.noreply.github.com>
2025-09-25 12:02:51 +0530

    Update tryon.tsx

--- app/(tabs)/tryon.tsx
+++ app/(tabs)/tryon.tsx
@@ -3,10 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, Linking, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 // Supabase credentials
-const supabaseUrl = "https://xetomtmbtiqwfisynrrl.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldG9tdG1idGlxd2Zpc3lucnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDg5NDMsImV4cCI6MjA3MjkyNDk0M30.eJNpLnTwzLyCIEVjwSzh3K1N4Y0mA9HV914pY6q3nRo";
-
+const supabaseUrl = "Supabase URL";
+const supabaseAnonKey = "Anon KEY";
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // A placeholder image to use when an image URL is not available
